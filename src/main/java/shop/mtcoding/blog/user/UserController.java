cbdcc2ffc8ac4b2692e5d6775ce996ad1290d313
kimseongjae <stwin755@gmail.com>
2024-02-14 19:50:35 +0900

    회원가입 구현하기

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,10 +1,23 @@
 package shop.mtcoding.blog.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor // final이 붙은 것 생성자 만들어줌
 public class UserController {
+
+    private final UserRepository userRepository; // final 변수는 반드시 초기화 돼야함
+
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO joinDTO) {
+
+        userRepository.save(joinDTO); // 모델에 위임하기
+
+        return "redirect:/loginForm";
+    }
 
     @GetMapping("/joinForm")
     public String joinForm() {
---
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -0,0 +1,29 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository // IoC에 new
+public class UserRepository {
+
+    // DB에 접근할 수 있는 매니저 객체
+    // 스프링이 만들어서 IoC에 넣어둔다.
+    // DI에서 꺼내 쓰기만 하면 된다.
+    private EntityManager em;
+
+    // 생성자 주입 (DI)
+    public UserRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Transactional // DB에 write할 때는 필수
+    public void save(UserRequest.JoinDTO joinDTO) {
+        Query query = em.createNativeQuery("insert into user_tb(username, password, email, created_at) values(?,?,?,now())");
+        query.setParameter(1, joinDTO.getUsername());
+        query.setParameter(2, joinDTO.getPassword());
+        query.setParameter(3, joinDTO.getEmail());
+        query.executeUpdate();
+    }
+}
