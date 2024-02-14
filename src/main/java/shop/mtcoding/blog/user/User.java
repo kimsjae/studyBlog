64cd2bd2d1ee4528794c63a2d588acbedf9eb67f
kimseongjae <stwin755@gmail.com>
2024-02-14 19:24:23 +0900

    유저 테이블 만들기

---
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -0,0 +1,19 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "user_tb")
+public class User {
+    @Id // PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime createdAt; // 카멜표기법으로 만들면 DB는 언더스코어기법으로 만들어진다.
+}
